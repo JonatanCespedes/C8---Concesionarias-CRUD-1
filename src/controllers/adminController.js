8b03125772df4cbfd74bdcca1508a8ce60e8b999
Jonatan Cespedes <retratos.jonatan@gmail.com>
2021-08-19 20:32:06 -0300

    Form admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,1 +1,23 @@
-const { getSucursales } = require('../data/dataBase')
+const { getSucursales, getAutos } = require('../data/dataBase');
+
+module.exports = {
+    index: (req, res) => {
+        res.render("admin/adminIndex")
+    },
+    sucursales: (req, res) => {
+        res.render('admin/adminSucursales', {
+            sucursales: getSucursales,
+            autos: function (idSucursal){
+                return getAutos.filter(auto => {
+                    return auto.sucursal === idSucursal
+                })
+            }
+        })
+    },
+    agregarSucursal: (req, res) => {
+        res.render('admin/agregarSucursal')
+    },
+    crearSucursal: (req, res) => {
+
+    },
+}
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,10 +1,18 @@
 let express = require('express');
 let router = express.Router();
-let controller = require('../controllers/adminController')
+let { 
+    index, 
+    agregarSucursal, 
+    sucursales, 
+    crearSucursal } = require('../controllers/adminController')
 
 /* Get - /index */
+router.get('/', index)
 /* get - /sucursales - listar sucursales */
+router.get('/sucursales', sucursales)
 /* get - /agregarSucursal - formulario de carga */
+router.get('/agregarSucursal', agregarSucursal)
 /* post - /agregarSucursal - recibir los datos y guardarlos en la BD */
+router.post('/agregarSucursal', crearSucursal)
 
 module.exports = router;
